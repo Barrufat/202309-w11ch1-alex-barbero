87c36c3040712b15e3530390bd89a3f2c451d4fb
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-18 14:05:25 +0100

    Modify debug attached

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const connectToDatabase = async (mongoUrl: string) => {
   try {
     await mongoose.connect(mongoUrl);
-    mongoose.set("debug", true);
+    mongoose.set("debug", false);
     console.log(chalk.blue("Connected to database"));
   } catch {
     console.log(chalk.red("Not connected to database"));
