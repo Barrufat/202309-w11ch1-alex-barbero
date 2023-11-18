a1a31eb1011c83f6354ceeafa376009b0ab6cc4b
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-18 16:33:42 +0100

    Add robots type

---
+++ src/features/robots/types.ts
@@ -0,0 +1,9 @@
+export type Grades = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10";
+
+export interface RobotsStructure {
+  _id: "a";
+  name: string;
+  image: string;
+  velocity: Grades;
+  resistance: Grades;
+}
