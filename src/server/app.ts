55921191bfe42d4600cebf8a06dbcef20a300e65
Alex Barbero <barrufat@hotmail.com>
2023-11-19 20:29:25 +0100

    Add helmet to app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import chalk from "chalk";
+import helmet from "helmet";
 
 export const app = express();
 app.disable("x-powered-by");
+app.use(helmet());
 
 export const startServer = (port: number) => {
   app.listen(port, () => {
