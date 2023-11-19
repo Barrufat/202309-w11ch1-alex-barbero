62beae79b93cdec18a720ca16a7d94d4a8326559
Alex Barbero <barrufat@hotmail.com>
2023-11-19 20:19:04 +0100

    Add cors to the index router

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,10 +4,13 @@ import { app } from "./app.js";
 import pingRouter from "../features/ping/router/PingRouter.js";
 import robotRouter from "../features/robots/router/robotRouter.js";
 import notFound from "../features/robots/middleware/errorMiddleware.js";
+import cors from "cors";
 
 app.use(express.json());
 
 app.use(morgan("dev"));
+
+app.use(cors());
 
 app.use("/", pingRouter);
 
